972d177af4e3e4906a907684840100dd74a05a9f
wilZero0 <wilianmedeiros0@gmail.com>
2025-07-07 22:12:28 -0300

    Add files via upload

---
+++ Aula01Py-main/Aula01Py-main/Aula de Python/Aula03/add_objeto.py
@@ -0,0 +1,9 @@
+numero = []
+print(numero)
+addnumero = int(input('Digite um numero: '))
+numero.append(addnumero)
+addnumero = int(input('Digite outro numero: '))
+numero.append(addnumero)
+soma = numero[0] + numero[1]
+print(numero)
+print(soma)
